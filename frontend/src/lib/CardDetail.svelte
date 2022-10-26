fa23512db6b8abd7190edf45607d4d84d4ec3b6c
Kaitaku Takeda <katakeda32@gmail.com>
2022-10-25 22:11:55 -0700

    Update style

--- frontend/src/lib/CardDetail.svelte
+++ frontend/src/lib/CardDetail.svelte
@@ -30,64 +30,95 @@
 </script>
 
 <div class="card-detail">
-  <div>
+  <div class="card-detail-group card-detail-title">
     {#if editingTitle}
-      <input type="text" value={ticket.title} />
-      <button on:click={() => (editingTitle = false)}>Cancel</button>
-      <button on:click={() => (editingTitle = false)}>Save</button>
+      <div class="card-detail-label-group">
+        <input type="text" value={ticket.title} />
+      </div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingTitle = false)}>Cancel</button>
+        <button on:click={() => (editingTitle = false)}>Save</button>
+      </div>
     {:else}
-      <span>{ticket.title}</span>
-      <button on:click={() => (editingTitle = true)}>Edit</button>
+      <div class="card-detail-label-group"><span>{ticket.title}</span></div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingTitle = true)}>Edit</button>
+      </div>
     {/if}
   </div>
-  <div>
-    <span>Description:</span>
+  <div class="card-detail-group card-detail-desc">
+    <span>Description: </span>
     {#if editingDescription}
-      <textarea cols="30" rows="10">{ticket.description}</textarea>
-      <button on:click={() => (editingDescription = false)}>Cancel</button>
-      <button
-        on:click={() => {
-          editingDescription = false;
-          updateTicket();
-        }}>Save</button
-      >
+      <div class="card-detail-label-group">
+        <textarea>{ticket.description}</textarea>
+      </div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingDescription = false)}>Cancel</button>
+        <button
+          on:click={() => {
+            editingDescription = false;
+            updateTicket();
+          }}>Save</button
+        >
+      </div>
     {:else}
-      <span>{ticket.description}</span>
-      <button on:click={() => (editingDescription = true)}>Edit</button>
+      <div class="card-detail-label-group">
+        <span>{ticket.description}</span>
+      </div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingDescription = true)}>Edit</button>
+      </div>
     {/if}
   </div>
-  <div>
-    <span>Assigned To:</span>
+  <div class="card-detail-group">
+    <span>Assigned To: </span>
     {#if editingAssignedTo}
-      <select>
-        {#each availableUsers as user}
-          <option value={user.id} selected={user.id == ticket.assignedTo}
-            >{user.name}</option
-          >
-        {/each}
-      </select>
-      <button on:click={() => (editingAssignedTo = false)}>Cancel</button>
-      <button on:click={() => (editingAssignedTo = false)}>Save</button>
+      <div class="card-detail-label-group">
+        <select>
+          {#each availableUsers as user}
+            <option value={user.id} selected={user.id == ticket.assignedTo}
+              >{user.name}</option
+            >
+          {/each}
+        </select>
+      </div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingAssignedTo = false)}>Cancel</button>
+        <button on:click={() => (editingAssignedTo = false)}>Save</button>
+      </div>
     {:else}
-      <span>{ticket.assignedTo}</span>
-      <button on:click={() => (editingAssignedTo = true)}>Edit</button>
+      <div class="card-detail-label-group">
+        <span>{ticket.assignedTo}</span>
+      </div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingAssignedTo = true)}>Edit</button>
+      </div>
     {/if}
   </div>
-  <div>
-    <span>Status:</span>
+  <div class="card-detail-group">
+    <span>Status: </span>
     {#if editingStatus}
-      <select>
-        {#each statuses as status}
-          <option value={status.value} selected={status.value == ticket.status}
-            >{status.label}</option
-          >
-        {/each}
-      </select>
-      <button on:click={() => (editingStatus = false)}>Cancel</button>
-      <button on:click={() => (editingStatus = false)}>Save</button>
+      <div class="card-detail-label-group">
+        <select>
+          {#each statuses as status}
+            <option
+              value={status.value}
+              selected={status.value == ticket.status}>{status.label}</option
+            >
+          {/each}
+        </select>
+      </div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingStatus = false)}>Cancel</button>
+        <button on:click={() => (editingStatus = false)}>Save</button>
+      </div>
     {:else}
-      <span>{ticket.status}</span>
-      <button on:click={() => (editingStatus = true)}>Edit</button>
+      <div class="card-detail-label-group">
+        <span>{ticket.status}</span>
+      </div>
+      <div class="card-detail-button-group">
+        <button on:click={() => (editingStatus = true)}>Edit</button>
+      </div>
     {/if}
   </div>
 </div>
@@ -96,5 +127,32 @@
   .card-detail {
     display: flex;
     flex-direction: column;
+    margin-top: 40px;
+  }
+  .card-detail-group {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+  .card-detail-group > span {
+    width: 20%;
+  }
+  .card-detail-label-group {
+    display: flex;
+    flex-grow: 1;
+    margin-right: 5px;
+  }
+  .card-detail-title span {
+    font-size: 22px;
+  }
+  .card-detail-title input {
+    width: 100%;
+  }
+  .card-detail-desc textarea {
+    margin-left: 3px;
+    width: 100%;
+    height: 65px;
+    font-family: sans-serif;
+    resize: none;
   }
 </style>
