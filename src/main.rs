ace8010493953535cc468283cc6d60fd3c193ee0
Kaitaku Takeda <katakeda32@gmail.com>
2022-11-06 18:57:49 -0800

    Change host

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 mod api;
 mod model;
 mod repository;
-
-use std::env::{self, set_var};
-
-use actix_files::Files;
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use api::api_config;
 use repository::inmem::Inmem;
+use std::env::{self, set_var};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -24,9 +21,8 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .app_data(app_data.clone())
             .configure(api_config)
-            .service(Files::new("/", "./static/").index_file("index.html"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
