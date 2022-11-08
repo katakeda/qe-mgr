fb2d9e7e9e23a2899b1e5acfe352465d70b9313a
Kaitaku Takeda <katakeda32@gmail.com>
2022-11-07 23:39:01 -0800

    Add deploy files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod api;
 mod model;
 mod repository;
+use actix_files::Files;
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use api::api_config;
 use repository::inmem::Inmem;
@@ -21,6 +22,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .app_data(app_data.clone())
             .configure(api_config)
+            .service(Files::new("/", "./dist/").index_file("index.html"))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
